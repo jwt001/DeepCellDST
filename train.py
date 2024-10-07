d4f8b4877379ae71583bd871a7e5a966fe19a221
Stone <zyshi21@cse.cuhk.edu.hk>
2024-10-07 17:38:30 +0800

    update

--- train.py
+++ train.py
@@ -5,11 +5,13 @@ from __future__ import print_function
 import deepcell
 import torch
 import os
+from config import get_parse_args
 
 os.environ['CUDA_LAUNCH_BLOCKING'] = '1'
-DATA_DIR = './data/train'
+DATA_DIR = './data/lcm'
 
 if __name__ == '__main__':
+    args = get_parse_args()
     circuit_path = os.path.join(DATA_DIR, 'graphs.npz')
     num_epochs = 60
     
@@ -19,7 +21,7 @@ if __name__ == '__main__':
     print('[INFO] Create Model and Trainer')
     model = deepcell.Model()
     
-    trainer = deepcell.Trainer(model, distributed=True)
+    trainer = deepcell.Trainer(args, model, distributed=True)
     trainer.set_training_args(loss_weight=[3.0, 1.0, 0.5], lr=1e-4, lr_step=50)
     print('[INFO] Stage 1 Training ...')
     trainer.train(num_epochs, train_dataset, val_dataset)
